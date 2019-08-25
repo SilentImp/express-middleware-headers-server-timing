e2947a835d9119ae764eff73f3ccb9997c959315
Anton Nemtsev <thesilentimp@gmail.com>
2019-08-25 22:52:44 +0200

    tests added

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  displayName: {
+    name: "Server Timing",
+    color: "blue"
+  },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      lines: 80
+    }
+  }
+};
